eb7274d954bf6eff6dcfc29a2bd51cec068e5530
Ondra Nejedlý <jsmesami@gmail.com>
2016-02-16 12:15:40 +0100

    Version 0.9

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@
 
 from setuptools import setup, find_packages
 
+# TODO: This needs more love.
+
 setup(
     name='naovoce',
     version='0.9',
+    url='https://github.com/jsmesami/naovoce.git',
     author='Ondra Nejedlý',
+    license='MIT',
     packages=find_packages(),
     scripts=[
         'src/manage.py'
