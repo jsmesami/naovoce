5003594eef419c026110eaf66575e7edf0743d02
Ondra Nejedlý <jsmesami@gmail.com>
2016-02-26 08:45:25 +0100

    Add token authorisation to API

--- src/naovoce/api/urls.py
+++ src/naovoce/api/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, url, include
+from rest_framework.authtoken import views
+
 from .views import api_root
 
 
@@ -9,4 +11,5 @@ urlpatterns = patterns(
     url(r'^herbarium/', include('fruit.herbarium.api.urls')),
     url(r'^users/', include('user.api.urls')),
     url(r'^images/', include('gallery.api.urls')),
+    url(r'^token/', views.obtain_auth_token),
 )
--- src/naovoce/settings/base.py
+++ src/naovoce/settings/base.py
@@ -284,4 +284,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
