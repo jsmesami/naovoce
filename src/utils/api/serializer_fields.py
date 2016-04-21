7e94ffb390953e1f0be46db83411269e087c3802
Ondra Nejedlý <jsmesami@gmail.com>
2016-04-21 17:08:28 +0200

    Revert CachedHyperlinkedIdentityField to original @flaiming native dict solution, because of significant performance boost.

--- src/utils/api/serializer_fields.py
+++ src/utils/api/serializer_fields.py
@@ -1,6 +1,5 @@
 import sys
 
-from django.core.cache import caches
 from rest_framework.serializers import HyperlinkedIdentityField
 
 
@@ -10,17 +9,18 @@ class CachedHyperlinkedIdentityField(HyperlinkedIdentityField):
     We save a ton of time by pre-computing the URL the first time it's
     accessed, to save calling reverse potentially thousands of times
     per request.
+    nb. we are not using cache here because of performance.
     """
     ID_TOKEN = str(sys.maxsize)
-    cache = caches['fruit']
+    URL_CACHE = {}
 
     def to_representation(self, value):
         cache_key = self.view_name
-        url = self.cache.get(cache_key)
+        url = self.URL_CACHE.get(cache_key)
         if not url:
             real_id = value.id
             value.id = self.ID_TOKEN
             url = super().to_representation(value)
-            self.cache.set(cache_key, url)
+            self.URL_CACHE[cache_key] = url
             value.id = real_id
         return url.replace(self.ID_TOKEN, str(value.id))
