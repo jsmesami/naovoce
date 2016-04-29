0ab9ba3be82eee3aa7994183f4c567b9fdb6ca9c
Ondra Nejedlý <jsmesami@gmail.com>
2016-04-29 21:29:01 +0200

    Add "Support us" static page
    TODO: English translation

--- src/naovoce/urls.py
+++ src/naovoce/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import ugettext_lazy as _
+from django.views.generic.base import RedirectView
 from django.views.generic import TemplateView
 
 import staticpage.views
@@ -38,8 +39,11 @@ urlpatterns += i18n_patterns(
     '',
     url(r'^$', views.home_view, name='home'),
     url(_(r'^map/$'), views.map_view, name='map'),
-    url(_(r'^about-us/$'), staticpage.views.static_view,
-        dict(template_name='staticpage/about.html'), name='about'),
+    url(_(r'^about-us/$'), RedirectView.as_view(permanent=True, pattern_name='team')),
+    url(_(r'^our-team/$'), staticpage.views.static_view,
+        dict(template_name='staticpage/team.html'), name='team'),
+    url(_(r'^support-us/$'), staticpage.views.static_view,
+        dict(template_name='staticpage/support.html'), name='support'),
     url(_(r'^codex/$'), staticpage.views.static_view,
         dict(template_name='staticpage/codex.html'), name='codex'),
 )
