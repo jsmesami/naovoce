eb4c2e22788a1fce7244a4398a7543dac7c46d39
Ondra Nejedlý <jsmesami@gmail.com>
2016-05-19 16:08:52 +0200

    Simplify latest entries on homepage

--- src/naovoce/views.py
+++ src/naovoce/views.py
@@ -5,49 +5,14 @@ from django.shortcuts import render
 
 from blog.models import BlogPost
 from fruit.models import Kind, Fruit
-from comments.models import Comment
 from gallery.models import Image
 from user.utils import pickers_counts_context
 
 
 def home_view(request):
-    fruits_qs = Fruit.objects.valid()\
-        .order_by('-created')\
-        .select_related('kind', 'user')
-
-    comm_qs = Comment.objects\
-        .order_by('-created')\
-        .prefetch_related('author', 'content_object')[:6]
-
-    desc_qs = fruits_qs.exclude(description='')[:6]
-
-    comm = [
-        dict(
-            url=c.get_absolute_url(),
-            author=c.author.get_short_name(),
-            author_url=c.author.get_absolute_url(),
-            text=c.text,
-            complaint=c.complaint,
-            time=c.created,
-        )
-        for c in comm_qs
-    ]
-
-    desc = [
-        dict(
-            url=d.get_absolute_url(),
-            author=d.user.get_short_name(),
-            author_url=d.user.get_absolute_url(),
-            text=d.description,
-            time=d.created,
-        )
-        for d in desc_qs
-    ]
-
     context = {
         'blogposts': BlogPost.objects.public(),
-        'fruits': fruits_qs,
-        'comments': sorted(comm + desc, key=lambda x: x['time'], reverse=True),
+        'fruit': Fruit.objects.valid().order_by('-created').select_related('kind', 'user'),
         'images': Image.objects.filter(gallery_ct=ContentType.objects.get_for_model(Fruit)),
     }
 
