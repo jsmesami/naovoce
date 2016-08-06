427db8695e7098d31db24e65505dc8ca18808a71
Ondra Nejedlý <jsmesami@gmail.com>
2016-08-06 19:15:42 +0200

    Add informational message when registering with malformed email domain instead of just 500.

--- src/user/adapters.py
+++ src/user/adapters.py
@@ -1,3 +1,10 @@
+import smtplib
+import logging
+
+from django.core.mail import mail_managers
+from django.utils.translation import ugettext
+from django.contrib import messages
+
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
@@ -6,11 +13,33 @@ from rest_framework.reverse import reverse
 from user.models import FruitUser
 
 
+logger = logging.getLogger(__name__)
+
+
 class AccountAdapter(DefaultAccountAdapter):
-    """
-    Allauth account adapter that does not use 'django.contrib.messages' if used by API.
-    """
+
+    def send_confirmation_mail(self, request, emailconfirmation, signup):
+        """
+        Do not just fail for typo in recipient email.
+        """
+        try:
+            super().send_confirmation_mail(request, emailconfirmation, signup)
+        except smtplib.SMTPRecipientsRefused as e:
+            error_msg = ugettext(
+                'Confirmation email has NOT been sent to {} (recipient refused).'
+            ).format(emailconfirmation.email_address.email)
+            logger.error(error_msg)
+            self.add_message(
+                request,
+                messages.constants.ERROR,
+                error_msg + ugettext('\nAdministrators have been notified.'),
+            )
+            mail_managers(error_msg, str(e))
+
     def add_message(self, request, *args, **kwargs):
+        """
+        Do not use 'django.contrib.messages' if used by API.
+        """
         if not request.path.startswith(reverse('api:root')):
             super().add_message(request, *args, **kwargs)
 
