62951dba2d1d4430e853aabba2536d078ef99448
Ondra Nejedlý <jsmesami@gmail.com>
2016-08-10 16:49:49 +0200

    More finetuning

--- src/utils/templatetags/coords.py
+++ src/utils/templatetags/coords.py
@@ -10,7 +10,7 @@ DMS_FMT = '{}\u00B0{}\u2032{}\u2033{dir}'
 
 
 @register.simple_tag
-def dms_coords(lt, lg, fmt='{lt},&nbsp;{lg}', prec=2):
+def dms_coords(lt, lg, fmt='{lt};&nbsp;{lg}', prec=2):
     return fmt.format(
         lt=DMS_FMT.format(*dd2dms(lt, prec), dir='S' if lt < 0 else 'N'),
         lg=DMS_FMT.format(*dd2dms(lg, prec), dir='W' if lg < 0 else 'E'),
