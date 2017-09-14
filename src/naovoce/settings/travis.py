770c185954c94e6a4e37add5e53854e1bb5000c5
Petr Dlouhý <petr.dlouhy@email.cz>
2017-09-14 12:26:52 +0200

    add Travis configuration; fix tests

---
+++ src/naovoce/settings/travis.py
@@ -0,0 +1,30 @@
+# Save this file as local.py
+from .base import *
+
+SECRET_KEY = "faketestkey"
+
+# Set correct credentials for production
+SERVER_EMAIL = ''
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'travis_ci_test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+RECAPTCHA_PUBLIC_KEY = '****'
+RECAPTCHA_PRIVATE_KEY = '****'
+RECAPTCHA_USE_SSL = True
+
+NEWSLETTER_API_KEY = '****'
+NEWSLETTER_DEFAULT_LIST_ID = '****'
+
+SECURE_SSL_REDIRECT = False
