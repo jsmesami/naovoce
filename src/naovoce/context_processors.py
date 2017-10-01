f7bcbc38400d99299770f55419bfa646e6d8c07c
Ondra Nejedlý <jsmesami@gmail.com>
2017-10-01 12:33:20 +0200

    Fix EU cookie for staging

--- src/naovoce/context_processors.py
+++ src/naovoce/context_processors.py
@@ -1,7 +1,9 @@
 from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
 
 
 def common(request):
     return {
-        'DEBUG': settings.DEBUG
+        'DEBUG': settings.DEBUG,
+        'DOMAIN': get_current_site(request).domain,
     }
