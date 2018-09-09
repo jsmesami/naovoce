be9ea5cf84759c55c0f8ff188d83c3767ff23ba5
Ondra Nejedlý <jsmesami@gmail.com>
2018-09-09 20:46:44 +0200

    Delete old models

--- src/gallery/managers.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class GalleryManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().prefetch_related('images').select_related('cover_image')
--- src/gallery/settings.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-# Iterable of lowercase model names, whose instances are allowed to store images
-# POSTed by authenticated users (default is empty list).
-GALLERY_PUBLIC_CONTAINERS = getattr(settings, 'GALLERY_PUBLIC_CONTAINERS', [])
-
-# Integer number of maximum bytes available for image storage (default is 5MB).
-GALLERY_IMAGE_MAX_FILESIZE = getattr(settings, 'GALLERY_IMAGE_MAX_FILESIZE', 5 * 1024 * 1024)
