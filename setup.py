e1ffb0ebbb2c3921369443ed965b010b51136248
Ondra Nejedlý <jsmesami@gmail.com>
2018-09-10 21:12:22 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,29 @@
 
 from setuptools import setup, find_packages
 
-# TODO: This needs more love.
+with open("requirements/base.txt") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+with open("requirements/test.txt") as f:
+    TEST_REQUIREMENTS = f.read().splitlines()
 
 setup(
-    name='naovoce',
-    version='1.0a',
-    url='https://github.com/jsmesami/naovoce.git',
-    author='Ondra Nejedlý',
-    license='MIT',
+    name="naovoce",
+    version="2.0.0a",
+    url="https://github.com/jsmesami/naovoce.git",
+    author="Ondřej Nejedlý",
+    author_email="software@na-ovoce.cz",
     packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    tests_require=TEST_REQUIREMENTS,
+    description="Na-ovoce.cz site backend",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
     scripts=[
         'src/manage.py'
     ],
